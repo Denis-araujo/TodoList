c3862ffe1fbf5dc04dda12a6e39cd80c28f38813
Denis Gomes <denisgomes938@gmail.com>
2022-11-20 16:09:43 -0300

    página de list finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,42 @@
 import { Header } from "./components/Header/Header";
+import { ClipboardText, PlusCircle } from "phosphor-react";
+
+import styles from "./App.module.css";
 
 export function App() {
   return (
-    <main>
+    <main className={styles.container}>
       <Header />
+
+      <div className={styles.containerContent}>
+        <header className={styles.headerForm}>
+          <form>
+            <input type="text" placeholder="Adicione uma tarefa" />
+            <button>
+              Criar
+              <PlusCircle size={16} />
+            </button>
+          </form>
+        </header>
+
+        <div className={styles.content}>
+          <header>
+            <span>
+              Tarefas criadas <span className={styles.counter}>0</span>
+            </span>
+            <span>
+              Concluidas <span className={styles.counter}>0</span>
+            </span>
+          </header>
+
+          <div className={styles.todoList}>
+            <ClipboardText size={56} />
+
+            <p>Você ainda não tem tarefas cadastradas</p>
+            <p>Crie tarefas e organize seus itens a fazer</p>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
