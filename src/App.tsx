1f42b2d21b354f3cb67e27b0556a57cd53b4d196
Denis Gomes <denisgomes938@gmail.com>
2022-11-20 23:22:58 -0300

    app finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,93 @@ import { ClipboardText, PlusCircle } from "phosphor-react";
 
 import styles from "./App.module.css";
 import { Task } from "./components/Task/Task";
+import { FormEvent, useState } from "react";
+
+export interface listTaskProps {
+  id: number;
+  value: string;
+  isComplete: boolean;
+}
 
 export function App() {
+  const [listTask, setListTask] = useState<Array<listTaskProps>>([]);
+
+  const [newTask, setNewTask] = useState("");
+
+  function handleCreateNewTask(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const task = {
+      id: Math.random(),
+      isComplete: false,
+      value: newTask,
+    };
+
+    setListTask([...listTask, task]);
+    setNewTask("");
+  }
+
+  function removeTask(taskToRemove: number) {
+    const newListTask = listTask.filter((item) => item.id !== taskToRemove);
+
+    setListTask(newListTask);
+  }
+
+  function checkedTask(taskchecked: number) {
+    const taskchecked2 = listTask.findIndex((task) => task.id === taskchecked);
+
+    const newListTasks = listTask.map((task, indice) => {
+      if (indice === taskchecked2) {
+        const isComplete = task.isComplete;
+
+        return {
+          ...task,
+          isComplete: !isComplete,
+        };
+      }
+
+      return task;
+    });
+
+    setListTask(newListTasks);
+  }
+
+  function tasksCompleted() {
+    const taskscompleted2 = listTask.reduce(
+      (accumulator, currentValue) => {
+        if (currentValue.isComplete === true) {
+          accumulator.total++;
+        } else {
+          accumulator.total;
+        }
+
+        return accumulator;
+      },
+      {
+        total: 0,
+      }
+    );
+
+    return taskscompleted2.total;
+  }
+
+  const isListTaskEmpty = listTask.length === 0;
+  const isNewTaskEmpty = newTask.length === 0;
+
   return (
     <main className={styles.container}>
       <Header />
 
       <div className={styles.containerContent}>
         <header className={styles.headerForm}>
-          <form>
-            <input type="text" placeholder="Adicione uma tarefa" />
-            <button>
+          <form onSubmit={handleCreateNewTask}>
+            <input
+              type="text"
+              placeholder="Adicione uma tarefa"
+              value={newTask}
+              onChange={(e) => setNewTask(e.target.value)}
+            />
+            <button type="submit" disabled={isNewTaskEmpty}>
               Criar
               <PlusCircle size={16} />
             </button>
@@ -23,25 +99,42 @@ export function App() {
         <div className={styles.content}>
           <header>
             <span>
-              Tarefas criadas <span className={styles.counter}>0</span>
+              Tarefas criadas{" "}
+              <span className={styles.counter}>{listTask.length}</span>
             </span>
             <span>
-              Concluidas <span className={styles.counter}>0</span>
+              Concluidas{" "}
+              <span className={styles.counter}>
+                {isListTaskEmpty
+                  ? "0"
+                  : `${tasksCompleted()} de ${listTask.length}`}
+              </span>
             </span>
           </header>
 
-          <div className={styles.todoList}>
-            <Task />
-            <Task />
-            <Task />
-          </div>
+          {isListTaskEmpty ? (
+            <div className={styles.emptyTaskList}>
+              <ClipboardText size={56} />
 
-          {/* <div className={styles.emptyTaskList}>
-            <ClipboardText size={56} />
-
-            <p>Você ainda não tem tarefas cadastradas</p>
-            <p>Crie tarefas e organize seus itens a fazer</p>
-          </div> */}
+              <p>Você ainda não tem tarefas cadastradas</p>
+              <p>Crie tarefas e organize seus itens a fazer</p>
+            </div>
+          ) : (
+            <div className={styles.todoList}>
+              {listTask.map((task) => {
+                return (
+                  <Task
+                    key={task.value}
+                    id={task.id}
+                    value={task.value}
+                    isComplete={task.isComplete}
+                    onDeleteTask={removeTask}
+                    onCheckedTask={checkedTask}
+                  />
+                );
+              })}
+            </div>
+          )}
         </div>
       </div>
     </main>
--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,21 +1,42 @@
-import { Circle, Trash } from "phosphor-react";
+import { CheckCircle, Circle, Trash } from "phosphor-react";
 
 import styles from "./Task.module.css";
 
-export function Task() {
+interface TaskProps {
+  id: number;
+  value: string;
+  isComplete: boolean;
+  onDeleteTask: (taskToRemove: number) => void;
+  onCheckedTask: (taskChecked: number) => void;
+}
+
+export function Task({
+  value,
+  id,
+  onDeleteTask,
+  onCheckedTask,
+  isComplete,
+}: TaskProps) {
+  function handleRemoveTask(task: number) {
+    onDeleteTask(task);
+  }
+
+  function handleCheckedTask(task: number) {
+    onCheckedTask(task);
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.task}>
-        <Circle size={17} />
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
-          cupiditate laborum reiciendis harum commodi, explicabo, fugit eius
-          magnam deleniti illum odio dolore pariatur amet at quia itaque, quos
-          optio fugiat?
-        </p>
+        {isComplete ? (
+          <CheckCircle size={23} onClick={() => handleCheckedTask(id)} />
+        ) : (
+          <Circle size={23} onClick={() => handleCheckedTask(id)} />
+        )}
+        <p className={isComplete ? styles.taskCompleted : ""}>{value}</p>
       </div>
 
-      <Trash />
+      <Trash size={23} onClick={() => handleRemoveTask(id)} />
     </div>
   );
 }
